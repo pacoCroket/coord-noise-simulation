7bbbc98ee6c3074f20424af54ff3e4c5ac9db6c0
Paco Croket <croketproduction@gmail.com>
2020-02-27 00:38:57 +0100

    started navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,49 @@
-import React, {Component} from 'React';
+import React, { Component } from "react";
 
 class NavBar extends Component {
-    render() {
-        
-    }
-
+  render() {
+    return (
+      <div className="navbar">
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
